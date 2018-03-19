0c71d5b692f44a4dddbd26a54cb355848d57fa49
Devon C Oliver <dolive3@siu.edu>
2018-03-19 15:45:51 -0500

    Work in Progess-saved at office

---
+++ R Code/SR_Code.R
@@ -0,0 +1,2 @@
+setwd("c:/Users/olive/Google Drive/Alex_data_and_SAS_files")#sets working directory
+SR_data = data.frame(read.csv(file="c:/Users/olive/Google Drive/Alex_data_and_SAS_files/Sr_Code_data.csv"))#calls dataset
