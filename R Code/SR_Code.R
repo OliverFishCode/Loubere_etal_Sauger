40cbbcd39fdab87946dbb119470fe8459ad5dc0c
Devon C Oliver <dolive3@siu.edu>
2018-04-04 17:55:09 -0500

    updates with predictions

--- R Code/SR_Code.R
+++ R Code/SR_Code.R
@@ -1,24 +1,26 @@
 ########Initial workspace setup#######
 setwd("c:/Users/olive/Google Drive/Alex_data_and_SAS_files")#sets working directory 
 SR_data = data.frame(read.csv(file="c:/Users/olive/Google Drive/Alex_data_and_SAS_files/Sr_Code_data.csv"))#calls dataset 
-#SR_data = droplevels(SR_data[-which(SR_data$site == "utrib"),])
-#SR_data$value = log(SR_data$value)
+#SR_data = droplevels(SR_data[-which(SR_data$site == "utrib"),])# removes utrib to test effects on normality and homogeniety
+#SR_data$value = log(SR_data$value)# natural log of response if needed
 options(scipen=999)
 
 
 ########## Calls in Packages####### 
 library(car) #regession utilities
 library(lme4)# linearmodels 
+library(nlme)# non-linear and linear mixed effect models with ability to adjust covariance structure
 library(lmerTest)# added utility to linear models 
-library(lsmeans)
+library(emmeans)# adds SAS like lsmeans functionality
 library(dplyr)# data manipulator 
-library(moments)# used to calculate skewness and kurtosis 
-library(e1071)
+library(e1071)# addition math functions and utility; allows for adjustments in kurtosis and skewness calc. to match SAS
 
 
 
 ######### arrange the data############
 SR_data$month = factor(SR_data$month, levels = c("may", "june", "july", "aug", "sept", "oct"))# forces the ordering(levels) of month to be logical rather than alphabetical
+SR_data$year= factor(SR_data$year, levels = c("2013", "2014", "2015", "2016", "2017"))# forces the ordering(levels) of year and makes it descrete rather than continuous
+
 SR_data = arrange(SR_data,site,year,month)# sorts data set by site,month,year
 
 #########User specified functions########## 
@@ -34,20 +36,19 @@ Descriptive_stats = summarise(group_by(SR_data,site),
                               Median = median(value), 
                               Skewness = skewness(value, type = 2), 
                               Kurtosis = kurtosis(value, type = 2), 
-                              Skewmness = skewness(value), 
-                              Kurtosis = kurtosis(value), 
                               Fifth_percentile= quantile(value, probs = percentile[1], type = 2), 
                               Ninety_Fifth_percentile= quantile(value, probs = percentile[2], type = 2)) 
+
 ##########Test assumptions of normality and homoscedasticity- mix of proc univariate and glm bartlett test#########
 temp_norm = shapiro.test(SR_data$value)# normality test 
-P = data.frame(temp_norm$p.value) 
-W = data.frame(temp_norm$statistic) 
-Shapiro_wilks_norm = data.frame(W,P)# puts normality test into dataframe
+P = data.frame(temp_norm$p.value)# temporary variable to contain P
+W = data.frame(temp_norm$statistic)# temporary variable to contain w statistic
+Shapiro_wilks_norm = data.frame(W,P)# puts normality test output into dataframe
 colnames(Shapiro_wilks_norm) = c("W","P")# give variables logical names
 
 temp_var = bartlett.test(value ~ site, data = SR_data) #Bartletts homogeniety of variance test r; requires atleast 2 values per group 
-P_B = data.frame(temp_var$p.value)
-K = data.frame(temp_var$statistic)
+P_B = data.frame(temp_var$p.value)# temporary variable to contain P
+K = data.frame(temp_var$statistic)# temporary variable to contain k statistic
 Bartlett_Homogen_var = data.frame(K, P_B)# puts homogeniety test into dataframe
 colnames(Bartlett_Homogen_var) = c("K","P")# give variables logical names
 remove(P,W,temp_norm,temp_var,K,P_B)# cleans up work environment to point
@@ -56,20 +57,24 @@ remove(P,W,temp_norm,temp_var,K,P_B)# cleans up work environment to point
 Sr_site_lm = glm(value ~ site, SR_data, family = "gaussian")
 summary(Sr_site_lm)
 glm_sum = summary(Sr_site_lm)
-
-msr = (glm_sum$null.deviance - glm_sum$deviance)/(glm_sum$df.null - glm_sum$df.residual)
-mse = glm_sum$deviance/glm_sum$df.residual
-Model_F_stat = data.frame(msr/mse)
-P_or_F = data.frame(2 * pf(q=Model_F_stat, df1=(glm_sum$df.null - glm_sum$df.residual), df2=(glm_sum$df.residual), lower.tail=FALSE))
+resid_df = as.numeric(glm_sum$df.residual)
+null_df = as.numeric(glm_sum$df.null)
+msr = (glm_sum$null.deviance - glm_sum$deviance)/(null_df - resid_df)
+mse = glm_sum$deviance/resid_df
+Model_F_stat = msr/mse
+P_or_F = data.frame(2 * pf(q=Model_F_stat, df1=(null_df - resid_df), df2=(resid_df), lower.tail=FALSE))
 Model_F_test = data.frame(Model_F_stat,P_or_F)
 
 confint(Sr_site_lm, level=0.95)
-lsmeans::lsmeans(Sr_site_lm,pairwise~site)
-
+lsmeans(Sr_site_lm,pairwise~site)
 #####Prediction Intervals from glm#######
 preds = predict.glm(Sr_site_lm,type="link", se.fit = TRUE)
-
+critval <- 1.96 ## approx 95% CI
+upr <- preds$fit + (critval * preds$se.fit)
+lwr <- preds$fit - (critval * preds$se.fit)
+fit <- preds$fit
+preds = data.frame(fit,lwr,upr)
 #################Wilcoxon model##############
-kruskal.test(value~site, SR_data)
-pairwise.wilcox.test(SR_data$value, SR_data$site,p.adjust.method = "bonferroni" )
+#kruskal.test(value~site, SR_data)
+#pairwise.wilcox.test(SR_data$value, SR_data$site,p.adjust.method = "bonferroni" )
 
